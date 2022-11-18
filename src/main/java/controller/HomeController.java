3fdf5d4127af1960924fe1227a83aa616310c9be
YanggggAng <tassapon.ans@student.mahidol.edu>
2022-11-18 21:16:30 +0700

    init homecontroller

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HomeController {
+
+
+    @GetMapping("/api/home")
+    public String Get() {return "this is home page should have movie";}
+
+}
