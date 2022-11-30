54076ab07825e59b06ddf0aebbd8164b24844932
Tiosphere <loong_22@hotmail.com>
2022-11-30 15:31:14 +0700

    minor fix and finish platform view

--- src/main/java/tk/rottencucumber/backend/controller/HomeController.java
+++ src/main/java/tk/rottencucumber/backend/controller/HomeController.java
@@ -3,15 +3,15 @@ package tk.rottencucumber.backend.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import tk.rottencucumber.backend.model.GenreModel;
-import tk.rottencucumber.backend.model.MovieModel;
-import tk.rottencucumber.backend.model.PlatformModel;
+import tk.rottencucumber.backend.model.*;
 import tk.rottencucumber.backend.record.movie.MovieRecord;
 import tk.rottencucumber.backend.record.movie.MovieRecordBuilder;
 import tk.rottencucumber.backend.record.response.ObjectResponse;
 import tk.rottencucumber.backend.record.simple.SimpleRecord;
 import tk.rottencucumber.backend.record.simple.SimpleRecordBuilder;
+import tk.rottencucumber.backend.record.simple.SimpleRecordWithMoviesBuilder;
 import tk.rottencucumber.backend.service.*;
+import tk.rottencucumber.backend.util.MovieRecordTool;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,26 +21,29 @@ public class HomeController {
     private final MovieService movieService;
     private final GenreService genreService;
     private final PlatformService platformService;
+    private final LanguageService languageService;
     private final ActorService actorService;
     private final WriterService writerService;
 
-    public HomeController(MovieService movieService, GenreService genreService, PlatformService platformService, ActorService actorService, WriterService writerService) {
+
+    public HomeController(MovieService movieService, GenreService genreService, PlatformService platformService, LanguageService languageService, ActorService actorService, WriterService writerService) {
         this.movieService = movieService;
         this.genreService = genreService;
         this.platformService = platformService;
+        this.languageService = languageService;
         this.actorService = actorService;
         this.writerService = writerService;
     }
 
     @GetMapping("/movies")
-    public List<MovieRecord> allMovie() {
+    public List<MovieRecord> allMovies() {
         Iterable<MovieModel> all = movieService.getAll();
-        return getMovieRecords(all);
+        return MovieRecordTool.getMovieRecords(all);
     }
 
     @GetMapping("/movie/{slug}")
     public ObjectResponse movie(@PathVariable String slug) {
-        MovieModel model = movieService.getBySlug(slug);
+        MovieModel model = movieService.findBySlug(slug);
         if (model == null) {
             return new ObjectResponse(false, "Can't find movie with this name", null);
         }
@@ -58,13 +61,16 @@ public class HomeController {
     }
 
     @GetMapping("/genre/{slug}")
-    public List<MovieRecord> genre(@PathVariable String slug) {
-        Iterable<MovieModel> all = movieService.getByGenre(slug);
-        return getMovieRecords(all);
+    public ObjectResponse genre(@PathVariable String slug) {
+        GenreModel model = genreService.findBySlug(slug);
+        if (model == null) {
+            return new ObjectResponse(false, "Can't find genre with this name", null);
+        }
+        return new ObjectResponse(true, String.format("Successfully get genre %s", model.getName()), List.of(SimpleRecordWithMoviesBuilder.create(model)));
     }
 
     @GetMapping("/platforms")
-    public List<SimpleRecord> allPlatform() {
+    public List<SimpleRecord> allPlatforms() {
         Iterable<PlatformModel> all = platformService.getAll();
         List<SimpleRecord> list = new ArrayList<>();
         for (PlatformModel model : all) {
@@ -74,9 +80,12 @@ public class HomeController {
     }
 
     @GetMapping("/platform/{slug}")
-    public List<MovieRecord> platform(@PathVariable String slug) {
-        Iterable<MovieModel> all = movieService.getByPlatform(slug);
-        return getMovieRecords(all);
+    public ObjectResponse platform(@PathVariable String slug) {
+        PlatformModel model = platformService.findBySlug(slug);
+        if (model == null) {
+            return new ObjectResponse(false, "Can't find genre with this name", null);
+        }
+        return new ObjectResponse(true, String.format("Successfully get genre %s", model.getName()), List.of(SimpleRecordWithMoviesBuilder.create(model)));
     }
 
 //    private record ActorResponse(ActorRecord actor, List<Record> movies) {}
